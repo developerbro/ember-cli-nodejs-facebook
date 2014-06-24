7d3c9631a8cf42008b02e697f9e2211d1e94d9f2
developerbro <developerbro@gmail.com>
2014-06-24 21:42:21 +0800

    improved logout functionality

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -2,8 +2,10 @@ export default Ember.ObjectController.extend({
 	actions: {
 		logout: function() {
 			var self = this;
-			Em.$.post('api/v1/auth/disconnect', {}).done(function(response) {
+			Em.$.post('api/v1/auth/disconnect', {}).always(function(response) {
+				localStorage.clear();
 				self.get('session').invalidate();
+				location.reload();
 			});
 		}
 	}
